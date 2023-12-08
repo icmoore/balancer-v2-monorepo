9cbe104f36edba160380c32f7e2e2e9689de04db
icmoore <ic3moore@gmail.com>
2023-12-08 15:18:14 -0800

    deploy weighted pool sequence

---
+++ pkg/pool-weighted-pachira/test/Simple.test.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import { fp } from '@balancer-labs/v2-helpers/src/numbers';
+
+import TokenList from '@balancer-labs/v2-helpers/src/models/tokens/TokenList';
+import WeightedPool from '@balancer-labs/v2-helpers/src/models/pools/weighted/WeightedPool';
+
+import { itBehavesAsWeightedPool } from './Simple.behavior';
+import { createPool } from './Simple.behavior';
+import { WeightedPoolType } from '@balancer-labs/v2-helpers/src/models/pools/weighted/types';
+
+
+describe('Simple', function () {
+
+  context('createPool', () => {
+    createPool(3, WeightedPoolType.WEIGHTED_POOL);
+  });  
+
+  //context('for a 3 token pool', () => {
+  //  itBehavesAsWeightedPool(3, WeightedPoolType.WEIGHTED_POOL);
+  //});
+
+});
