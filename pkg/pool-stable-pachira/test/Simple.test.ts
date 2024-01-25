2e84088ce69a14a33bc53605174d49ea32b48b98
icmoore <ic3moore@gmail.com>
2024-01-24 18:24:09 -0800

    update

--- pkg/pool-stable-pachira/test/Simple.test.ts
+++ pkg/pool-stable-pachira/test/Simple.test.ts
@@ -1,51 +1,26 @@
-// scripts/deployAndInteract.ts
-import { ethers } from 'hardhat';
 import { expect } from 'chai';
+import { ethers } from 'hardhat';
 import { BigNumber, Contract } from 'ethers';
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-import { defaultAbiCoder } from '@ethersproject/abi';
 import { impersonateAccount, setBalance } from '@nomicfoundation/hardhat-network-helpers';
 
+import { deploy, deployedAt, getArtifact } from '@balancer-labs/v2-helpers/src/contract';
+import { sharedBeforeEach } from '@balancer-labs/v2-common/sharedBeforeEach';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
 
-import { Vault } from '../typechain/contracts/Vault';    
-import { MockAuthorizerAdaptorEntrypoint } from '../typechain/contracts/test/MockAuthorizerAdaptorEntrypoint';       
-import { MockAuthorizerAdaptorEntrypoint__factory } from '../typechain/factories/contracts/test/MockAuthorizerAdaptorEntrypoint__factory';     
-import { TimelockAuthorizer__factory } from '../typechain/factories/contracts/authorizer/TimelockAuthorizer__factory';     
-import { Vault__factory } from '../typechain/factories/contracts/Vault__factory';       
-
+import { BigNumberish, bn, fp, FP_ONE, FP_ZERO} from '@balancer-labs/v2-helpers/src/numbers';
+import { ZERO_ADDRESS } from '@balancer-labs/v2-helpers/src/constants';
 import { RawStablePoolDeployment } from '@balancer-labs/v2-helpers/src/models/pools/stable/types';
-import { toNormalizedWeights } from '@balancer-labs/balancer-js';
-import { MAX_UINT256, ZERO_ADDRESS } from '@balancer-labs/v2-helpers/src/constants';
-import { BigNumberish, fp, fpMul, bn, FP_ONE} from '@balancer-labs/v2-helpers/src/numbers';
-import { currentTimestamp, MONTH } from '@balancer-labs/v2-helpers/src/time';
+import { currentTimestamp, advanceTime, MONTH, WEEK, DAY } from '@balancer-labs/v2-helpers/src/time';
 import TokenList from '@balancer-labs/v2-helpers/src/models/tokens/TokenList';
-import {SingleSwap, SwapKind, FundManagement, JoinPoolRequest, WeightedPoolEncoder} from '@balancer-labs/balancer-js';
-import { deploy, getArtifact} from '@balancer-labs/v2-helpers/src/contract';
 import StablePool from '@balancer-labs/v2-helpers/src/models/pools/stable/StablePool';
 
-
-const NAME = 'Pachira Balancer Pool Token';
-const SYMBOL = 'BPT';
-const POOL_SWAP_FEE_PERCENTAGE = fp(0.01);
+import { Vault } from '../typechain/contracts/Vault';    
+import { MockAuthorizerAdaptorEntrypoint } from '../typechain/contracts/test/MockAuthorizerAdaptorEntrypoint';  
+import { MockAuthorizerAdaptorEntrypoint__factory } from '../typechain/factories/contracts/test/MockAuthorizerAdaptorEntrypoint__factory';     
+import { TimelockAuthorizer__factory } from '../typechain/factories/contracts/authorizer/TimelockAuthorizer__factory'; 
+import { Vault__factory } from '../typechain/factories/contracts/Vault__factory';    
 
 const numTokens = 3;
-const WEIGHTS = toNormalizedWeights([fp(30), fp(70), fp(5), fp(5)].slice(0, numTokens));
-const INITIAL_BALANCES = [fp(0.9), fp(1.8), fp(2.7), fp(3.6)];
-const TOKEN_SYMBOLS = ['USDC', 'DAI', 'USDT', 'BUSD'].slice(0, numTokens)
-const weights: BigNumberish[] = WEIGHTS.slice(0, numTokens);
-const initialBalances = INITIAL_BALANCES.slice(0, numTokens);
-const ZEROS = Array(3).fill(bn(0));
-const MAX_AUM_VALUE = fp(0.2);
-const MAX_YIELD_VALUE = fp(0.8);
-const AMPLIFICATION_PARAMETER = bn(200);
-
-
-const BASE_PAUSE_WINDOW_DURATION = MONTH * 3;
-const BASE_BUFFER_PERIOD_DURATION = MONTH;
-
-
-let tokens: TokenList;
-let allTokens: TokenList;
 let lp: SignerWithAddress;
 let owner: SignerWithAddress;
 let deployer: SignerWithAddress;
@@ -53,12 +28,16 @@ let recipient: SignerWithAddress;
 let vaultSigner: SignerWithAddress;
 let admin: SignerWithAddress;
 let authorizer: Contract;
+let initialBalances: BigNumberish[];
+let bptIndex: number;
+let pool: StablePool;
+let tokens: TokenList;
 let vault: Vault;
 let entrypoint: MockAuthorizerAdaptorEntrypoint;
-let pool: StablePool;
+
+const TOKEN_SYMBOLS = ['USDC', 'DAI', 'USDT', 'BUSD'].slice(0, numTokens)
 
 async function deployRawVault(): Promise<void>  {
-  //const entrypoint = await deploy('MockAuthorizerAdaptorEntrypoint');
   entrypoint = await new MockAuthorizerAdaptorEntrypoint__factory(deployer).deploy()
   authorizer = await new TimelockAuthorizer__factory(deployer).deploy(admin.address, ZERO_ADDRESS, entrypoint.address, MONTH)
   vault = await new Vault__factory(deployer).deploy(authorizer.address, ZERO_ADDRESS, 0, 0)
@@ -69,24 +48,20 @@ async function deployRawVault(): Promise<void>  {
 
 async function deployTokens(): Promise<void> {
     const tokenAmounts = fp(100);
-    allTokens = await TokenList.create(TOKEN_SYMBOLS, { sorted: true });
-    await allTokens.mint({ to: lp, amount: tokenAmounts });
-    await allTokens.approve({ to: vault.address, from: lp, amount: tokenAmounts }); 
-    tokens = allTokens.subset(numTokens);
+    tokens = await TokenList.create(TOKEN_SYMBOLS, { sorted: true });
+    await tokens.mint({ to: lp, amount: tokenAmounts });
+    await tokens.approve({ to: vault.address, from: lp, amount: tokenAmounts }); 
 }  
-
 
 async function deployPool(
   params: RawStablePoolDeployment = {},
   rates: BigNumberish[] = [],
   durations: number[] = []
 ): Promise<void> {
+  
   const rateProviders: Contract[] = [];
   const tokenRateCacheDurations: number[] = [];
   const exemptFromYieldProtocolFeeFlags: boolean[] = [];
-  let bptIndex: number;
-  let deployTimestamp: BigNumber;
-  let initialBalances: BigNumberish[];
 
   for (let i = 0; i < numTokens; i++) {
     rateProviders[i] = await deploy('v2-pool-utils/MockRateProvider');
@@ -105,27 +80,64 @@ async function deployPool(
     ...params,
   });
 
-  bptIndex = await pool.getBptIndex();
-  deployTimestamp = await currentTimestamp();
-  initialBalances = Array.from({ length: numTokens + 1 }).map((_, i) => (i == bptIndex ? 0 : fp(1 - i / 10)));
 }
 
-before('setup signers', async () => {
-  [ ,admin, lp, owner, recipient, deployer] = await ethers.getSigners();
-});  
+async function initPool(): Promise<void> {
+  bptIndex = await pool.getBptIndex();
+  initialBalances = Array.from({ length: numTokens + 1 }).map((_, i) => (i == bptIndex ? 0 : fp(1 - i / 10)));  
+  await pool.init({ initialBalances, recipient: lp });
+}  
 
-describe("PachiraWeightedPool", () => {
+async function swapPool(): Promise<void> {
+  const amountIn = fp(0.1);
 
-  context('Engage factory contract PachiraWeightedPool.sol', () => {
+  await tokens.mint({ to: lp, fp(100); });
+  await tokens.approve({ from: lp, to: pool.vault });
+
+  const amountOut = pool.swapGivenIn({ in: tokens.first, out: tokens.second, amount: amountIn, recipient });
+  console.log('amountIn '+amountIn);  
+  console.log('amountOut '+ amountOut);  
+}  
+
+
+async function poolInfo(context: string): Promise<void> {
+  const poolId = await pool.getPoolId();
+  const poolTokens = await pool.getTokens()
+
+  let message: string = "\n        Weighted pool info: " + context + "+\n";
+  message = message + '        ------------------------\n'
+  message = message + '        vault address: '+await vault.address+'\n'
+  message = message + '        balances: '+ await poolTokens.balances +'\n'
+  message = message + '        tkn addresses: '+ await poolTokens.tokens +'\n'
+  message = message + '        ------------------------\n'
+  console.log(message);  
+
+}  
+
+sharedBeforeEach('setup signers', async () => {
+  [, lp, owner, recipient, admin, deployer] = await ethers.getSigners();
+});
+
+describe("StablePool", () => {
+
+  context('Engage factory contract StablePool.sol', () => {
     const swapFeePercentage = fp(0.1);
     const tokenRates = Array.from({ length: numTokens }, (_, i) => fp(1 + (i + 1) / 10));
     
     it('swap given in', async () => {
       expect(true);
-      await deployRawVault()
-      await deployTokens();
-      await deployPool({ swapFeePercentage, amplificationParameter: AMPLIFICATION_PARAMETER }, tokenRates);
-    });           
- 
+      await deployRawVault();
+      await deployTokens()
+      await deployPool();
+      await initPool();
+      await poolInfo('post init');
+      await swapPool();
+      await poolInfo('post swap');
+    });   
+
   }); 
+
 });    
+
+
+
